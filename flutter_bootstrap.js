f90e1ea4a6eb64b5f2904ade1a8fb14e75886a9d
Logende <Logende@users.noreply.github.com>
2026-05-25 08:52:49 +0000

    deploy: 582af53c91b59a48ff865aade06ae90f89bfbfaa

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4019027945" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "592916525" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
